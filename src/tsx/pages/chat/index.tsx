bbf354658155233370676e18b086e955f2f0dbc3
Dávid J. Zarándi <hello@davidzarandi.eu>
2023-12-26 12:57:17 +0100

    feat: updated layout and routes

--- src/tsx/pages/chat/index.tsx
+++ src/tsx/pages/chat/index.tsx
@@ -1,32 +1,58 @@
 import { createSignal } from "solid-js";
-import Sidebar from "./sidebar";
+import { A } from "@solidjs/router";
 import { LeftPanelOpenIcon } from "../../icons/left-panel-open";
 import { LeftPanelCloseIcon } from "../../icons/left-panel-close";
 
 const Chat = () => {
-  const [isSidebarVisible, setSidebarVisibility] = createSignal(true);
+  const [isSidePanelCollapsed, setSidePanelCollapsion] = createSignal(false);
 
   return (
-    <>
-      <Sidebar isOpen={isSidebarVisible} />
-      <div class="-surface-1 main-content">
-        <div class="header">
+    <main
+      class="content-grid"
+      role="main"
+      data-side-panel-collapsed={isSidePanelCollapsed()}
+    >
+      <div class="surface side-panel">
+        <header class="header">
+          <button class="full-width">New chat</button>
+        </header>
+        <menu class="main">
+          {new Array(50).fill("").map((_, index) => (
+            <li class="full-width">
+              <button>Chat {index}</button>
+            </li>
+          ))}
+        </menu>
+        <footer class="footer menu">
+          <A class="full-width" href="/">
+            Sign out
+          </A>
+        </footer>
+      </div>
+      <div class="main-panel">
+        <header class="header">
           <button
-            class="sidebar-toggle"
-            type="button"
-            onClick={() => setSidebarVisibility((prevState) => !prevState)}
+            onClick={() => setSidePanelCollapsion((prevState) => !prevState)}
           >
-            {isSidebarVisible() ? (
+            {isSidePanelCollapsed() ? (
               <LeftPanelCloseIcon />
             ) : (
               <LeftPanelOpenIcon />
             )}
           </button>
-        </div>
-        <div class="content">content</div>
-        <div class="footer">footer</div>
+          <span>Chat name</span>
+        </header>
+        <ol class="main">
+          {new Array(50).fill("").map((_, index) => (
+            <li class="message">Message {index}</li>
+          ))}
+        </ol>
+        <footer class="footer chat">
+          <textarea class="message-input"></textarea>
+          <button class="send-button">Send</button>
+        </footer>
       </div>
-    </>
+    </main>
   );
 };
 
--- src/tsx/pages/landing.tsx
+++ src/tsx/pages/landing.tsx
@@ -2,19 +2,21 @@ import { A } from "@solidjs/router";
 
 const Landing = () => {
   return (
-    <>
-      <div class="sidebar">
-        <p>Welcome to</p>
-        <p>LLM Chat</p>
+    <main class="content-grid" role="main">
+      <div class="side-panel">
+        <p class="full-width">
+          <b>Welcome to</b>
+        </p>
+        <p class="full-width">LLM Chat</p>
       </div>
-      <div class="main-content">
+      <div class="main-panel">
         <div class="full-height">
           <A href="/chat">
             <p>Navigate me to chat</p>
           </A>
         </div>
       </div>
-    </>
+    </main>
   );
 };
 
